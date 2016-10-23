c45bb39f0f754a7b34dc4b234c3384ccc964de7e
Rong-En Fan <rafan@users.noreply.github.com>
2016-10-23 14:52:03 +0800

    Dont create unneeded symlink

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ cd
 for i in profile/dot.*; do
 	/bin/ln -fs ${i} ~/${i#profile/dot}
 done
-/bin/ln -fs profile/site-lisp ~/.site-lisp
-/bin/ln -fs profile/vim ~/.vim
+rm -f ~/.site-lisp && /bin/ln -fs profile/site-lisp ~/.site-lisp
+rm -f ~/.vim && /bin/ln -fs profile/vim ~/.vim
 /bin/ln -fs profile/dot.Xdefaults .Xresources
