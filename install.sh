5f71c0cd6ac672b4365eab84c7900651a33fa67c
Rong-En Fan <rafan@users.noreply.github.com>
2025-04-02 11:03:33 +0800

    fix: relative path

--- install.sh
+++ install.sh
@@ -3,13 +3,20 @@
 # $Id: install.sh 2768 2007-06-17 09:09:29Z rafan $
 #
 
-cd
+set -o errexit
 
-for i in profile/dot.*; do
-	/bin/ln -fs ${i} ~/${i#profile/dot}
+# Get base directory, relative to $HOME
+basedir=$(dirname $0)
+basedir=$(cd $basedir; pwd)
+basedir=${basedir#$HOME/}
+
+cd ~
+
+for i in $basedir/dot.*; do
+	/bin/ln -fs ${i} ~/${i#$basedir/dot}
 done
-rm -f ~/.vim && /bin/ln -fs profile/vim ~/.vim
+rm -f ~/.vim && /bin/ln -fs $basedir/vim ~/.vim
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-/bin/ln -fs ../profile/ssh/config ~/.ssh/config
+/bin/ln -fs $basedir/ssh/config ~/.ssh/config
